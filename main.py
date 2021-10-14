9c2e396b788eb6dfbc904e85c7b6e59e5c5fe247
Cijin Joseph John <66782050+cijinjosephjohn@users.noreply.github.com>
2021-10-14 14:40:33 +0530

    Changed main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ win.setup(700,700)
 win.title("Fidget Spinner")
 turtle.color("Yellow")
 turtle.write("FIDGET SPINNER" , align= "right" , font=("Arial" , 20 , "bold") )
-turtle.color("Orange")
+turtle.color("green")
 turtle.write("USE SPACE BUTTON TO SPEED UP " , align= "left" , font=("Arial" , 15 , "bold") )
 
 
@@ -15,107 +15,56 @@ obj1.speed(8)
 obj1.color("white")
 obj1.width(3)
 obj1.hideturtle()
-win.tracer(False)
-speed=[0] 
+obj1.goto(0.00 , -200.00)
+speed= {"turn" :0}
 length = 100
 rad = 120
-dot_rad = 30 
-angle = 0
-dot = 5
-obj1.goto(0.00 , -200.00)
-obj1.right(angle)
-obj1.forward(length)
-obj1.dot(rad , "red")
-obj1.dot(dot,"yellow")
-obj1.back(length)
-obj1.right(90)
-
-
-obj1.right(angle)
-obj1.forward(length)
-obj1.dot(rad , "green")
-obj1.dot(dot,"yellow")
-obj1.back(length)
-obj1.right(90)
-
-
-obj1.right(angle)
-obj1.forward(length)
-obj1.dot(rad , "blue")
-obj1.dot(dot,"yellow")
-obj1.back(length)
-obj1.right(90)
-
-
-obj1.right(angle)
-obj1.forward(length)
-obj1.dot(rad , "indigo")
-obj1.dot(dot,"yellow")
-obj1.back(length)
-obj1.right(90)
-
-
-
-
-
-
-
-
-
 def spinner():
     obj1.clear()
-    angle = speed[0]/10
-    obj1.goto(0.00 , -200.00)
+    angle = speed["turn"]/10
     obj1.right(angle)
     obj1.forward(length)
     obj1.dot(rad , "red")
-    obj1.dot(dot_rad,"yellow")
+    obj1.dot(20,"yellow")
     obj1.back(length)
-    obj1.right(90)
+    obj1.right(120)
 
 
-    obj1.right(angle)
+
+    
     obj1.forward(length)
     obj1.dot(rad , "green")
-    obj1.dot(dot_rad,"yellow")
+    obj1.dot(20,"yellow")
     obj1.back(length)
-    obj1.right(90)
+    obj1.right(120)
 
 
-    obj1.right(angle)
+    
     obj1.forward(length)
     obj1.dot(rad , "blue")
-    obj1.dot(dot_rad,"yellow")
+    obj1.dot(20,"yellow")
     obj1.back(length)
-    obj1.right(90)
-
-
-    obj1.right(angle)
-    obj1.forward(length)
-    obj1.dot(rad , "indigo")
-    obj1.dot(dot_rad,"yellow")
-    obj1.back(length)
-    obj1.right(90)
+    obj1.right(120)
+    
 
 
 
 
 def speedingup():
-    speed[0]  += 25 
+    speed["turn"] += 40 
 def  roatate():
-    if speed[0] !=0 :
-        speed[0] -= 1
+    if speed["turn"] !=0 :
+        speed["turn"] -= 1
     spinner()
     win.ontimer(roatate,20)
 def main():
     win.tracer(False)
-    win.listen()
     win.onkey(speedingup,'space')
-   
+    win.listen()
     roatate()
     
     
     turtle.done()
 
-if __name__=="__main__":  
+if _name=="main_":
     main()
